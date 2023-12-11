4f26f0972f70af7b86c6dfe79d58b1e0febda439
Yashar Zafari <yasharez@gmail.com>
2023-12-11 15:53:02 -0800

    lifted mongoose connection out of model files

---
+++ backend/models/db.js
@@ -0,0 +1,34 @@
+/**
+ * Yashar Zafari
+ * 12/06/2023
+ * 
+ * Database file to connect to MongoDB
+ */
+
+'use strict';
+
+import { createRequire } from 'module';
+const require = createRequire(import.meta.url);
+const dotenv = require('dotenv');
+
+// Load environment variables
+dotenv.config({ path: '../.env' });
+
+import mongoose from 'mongoose';
+
+console.log(process.env.MONGODB_CONNECT_STRING);
+
+// Connect to database
+mongoose.connect(process.env.MONGODB_CONNECT_STRING);
+const db = mongoose.connection;
+
+// Confirm conection
+db.once('open', (error) => {
+    if (error) {
+        console.log(error);
+    } else {
+        console.log('Connection successful');
+    };
+});
+
+export { mongoose };
---
+++ backend/models/picks-model.js
@@ -0,0 +1,6 @@
+/**
+ * Yashar Zafari
+ * 12/06/2023
+ * 
+ * Model file for picks collection in MongoDB
+ */
